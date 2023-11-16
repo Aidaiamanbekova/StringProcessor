fc2c27b83726810733e48c10a8e526c996bd12a1
Aidaiamanbekova <145114212+Aidaiamanbekova@users.noreply.github.com>
2023-11-16 14:45:41 +0600

    Add files via upload

---
+++ StringProcessor/src/StringProcessor.java
@@ -0,0 +1,144 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringProcessor {
+
+    public boolean isStrongPassword(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public int calculateDigits(String sentence) {
+        int digitCount = 0;
+        for (char c : sentence.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digitCount++;
+            }
+        }
+        return digitCount;
+    }
+
+    public int calculateWords(String sentence) {
+        String[] words = sentence.split("\\s+");
+        return words.length;
+    }
+
+    public double calculateExpression(String expression) {
+        return eval(expression);
+    }
+
+    // Helper method to evaluate arithmetic expressions
+    private double eval(String expression) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') {
+                    nextChar();
+                }
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < expression.length()) {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) {
+                        x += parseTerm();
+                    } else if (eat('-')) {
+                        x -= parseTerm();
+                    } else {
+                        return x;
+                    }
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) {
+                        x *= parseFactor();
+                    } else if (eat('/')) {
+                        x /= parseFactor();
+                    } else {
+                        return x;
+                    }
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor();
+                if (eat('-')) return -parseFactor();
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) {
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') {
+                        nextChar();
+                    }
+                    x = Double.parseDouble(expression.substring(startPos, this.pos));
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                return x;
+            }
+        }.parse();
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        StringProcessor stringProcessor = new StringProcessor();
+
+        // Test isStrongPassword
+        System.out.println("Test isStrongPassword:");
+        System.out.println(stringProcessor.isStrongPassword("StrongP@ssword")); // true
+        System.out.println(stringProcessor.isStrongPassword("Weakpassword123")); // false
+        System.out.println(stringProcessor.isStrongPassword("OnlyLowerCase")); // false
+        System.out.println(stringProcessor.isStrongPassword("1234567890")); // false
+        System.out.println(stringProcessor.isStrongPassword("!@#$%^&*()")); // false
+        // Test calculateDigits
+        System.out.println("\nTest calculateDigits:");
+        System.out.println(stringProcessor.calculateDigits("abc123")); // 3
+        System.out.println(stringProcessor.calculateDigits("NoDigitsHere")); // 0
+        System.out.println(stringProcessor.calculateDigits("456 789 123")); // 9
+        System.out.println(stringProcessor.calculateDigits("Special#Chars!12")); // 2
+        System.out.println(stringProcessor.calculateDigits("")); // 0
+
+        // Test calculateWords
+        System.out.println("\nTest calculateWords:");
+        System.out.println(stringProcessor.calculateWords("This is a sentence.")); // 4
+        System.out.println(stringProcessor.calculateWords("")); // 0
+        System.out.println(stringProcessor.calculateWords("   Extra   Spaces   ")); // 2
+        System.out.println(stringProcessor.calculateWords("OneWordOnly")); // 1
+        System.out.println(stringProcessor.calculateWords("Hello, World!")); // 2
+
+        // Test calculateExpression
+        System.out.println("\nTest calculateExpression:");
+        System.out.println(stringProcessor.calculateExpression("2 + 3 * (4 - 1)")); // 11.0
+        System.out.println(stringProcessor.calculateExpression("(10 + 2) / 3")); // 4.0
+        System.out.println(stringProcessor.calculateExpression("5 - 3 + 1")); //
+         }
+}
+
